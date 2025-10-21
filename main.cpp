d5f835d1966afc301a32ceb5f87e4737a15166e1
vincent <vincent.lextrait@gmail.com>
2025-10-20 17:37:34 -0700

    Push Mon Oct 20 17:37:34 PDT 2025

---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Copyright Metaspex - 2023
+// mailto:admin@metaspex.com
+//
+
+// This include is enough to embark all the standard services managing identity (plus more) from the Foundation Ontology.
+#include <hx2a/services/foundation.hpp>
